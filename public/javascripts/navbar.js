3d41113a5345d3a5b2b5ef5f3e4a105c59e86894
Andrew <acrisp013@gmail.com>
2016-04-24 06:22:02 -0400

    Updated logos, formated add form

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -5,7 +5,13 @@ $('#nav').affix({
   }
 });
 
-/* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
+$('#nav').affix({
+  offset: {
+    top: $('small_header').height()-$('#nav').height()
+  }
 });
+
+/* highlight the top nav as scrolling occurs */
+$('body').scrollspy({ target: '#nav' })
+
+/* smooth scrolling for scroll to top */
